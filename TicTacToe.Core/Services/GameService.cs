c76fa017074c2a55151adb84d51bc80d8351a05d
Jatin-CI0002 <jatin@caizin.com>
2022-10-10 13:00:38 +0530

    did some tweaks

--- TicTacToe.Core/Services/GameService.cs
+++ TicTacToe.Core/Services/GameService.cs
@@ -49,70 +49,6 @@ namespace TicTacToe.Core
             }
             return bestMove;
         }
-
-        // This function returns true if there are moves
-        // remaining on the board. It returns false if
-        // there are no moves left to play.
-        private static Boolean isMovesLeft(string[,] board)
-        {
-            for (int i = 0; i < 3; i++)
-                for (int j = 0; j < 3; j++)
-                    if (board[i, j] == "")
-                        return true;
-            return false;
-        }
-
-        // This is the evaluation function as discussed
-        // in the previous article ( http://goo.gl/sJgv68 )
-        private static int evaluate(string[,] board)
-        {
-            // Checking for Rows for X or O victory.
-            for (int row = 0; row < 3; row++)
-            {
-                if (board[row, 0] == board[row, 1] &&
-                    board[row, 1] == board[row, 2])
-                {
-                    if (board[row, 0] == player)
-                        return +10;
-                    else if (board[row, 0] == opponent)
-                        return -10;
-                }
-            }
-
-            // Checking for Columns for X or O victory.
-            for (int col = 0; col < 3; col++)
-            {
-                if (board[0, col] == board[1, col] &&
-                    board[1, col] == board[2, col])
-                {
-                    if (board[0, col] == player)
-                        return +10;
-
-                    else if (board[0, col] == opponent)
-                        return -10;
-                }
-            }
-
-            // Checking for Diagonals for X or O victory.
-            if (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])
-            {
-                if (board[0, 0] == player)
-                    return +10;
-                else if (board[0, 0] == opponent)
-                    return -10;
-            }
-
-            if (board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0])
-            {
-                if (board[0, 2] == player)
-                    return +10;
-                else if (board[0, 2] == opponent)
-                    return -10;
-            }
-
-            return 0;
-        }
-
         // This is the optimal function. It considers all
         // the possible ways the game can go and returns
         // the value of the board
@@ -124,7 +60,7 @@ namespace TicTacToe.Core
             //for computer move
             if (score == 10)
                 return score;
-            
+
             //for user move
             if (score == -10)
                 return score;
@@ -183,7 +119,69 @@ namespace TicTacToe.Core
                 }
                 return best;
             }
-        } 
-    }
+        }
+
+        private static int evaluate(string[,] board)
+        {
+            // Checking for Rows for X or O victory.
+            for (int row = 0; row < 3; row++)
+            {
+                if (board[row, 0] == board[row, 1] &&
+                    board[row, 1] == board[row, 2])
+                {
+                    if (board[row, 0] == player)
+                        return +10;
+                    else if (board[row, 0] == opponent)
+                        return -10;
+                }
+            }
+
+            // Checking for Columns for X or O victory.
+            for (int col = 0; col < 3; col++)
+            {
+                if (board[0, col] == board[1, col] &&
+                    board[1, col] == board[2, col])
+                {
+                    if (board[0, col] == player)
+                        return +10;
+
+                    else if (board[0, col] == opponent)
+                        return -10;
+                }
+            }
+
+            // Checking for Diagonals for X or O victory.
+            if (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])
+            {
+                if (board[0, 0] == player)
+                    return +10;
+                else if (board[0, 0] == opponent)
+                    return -10;
+            }
+
+            if (board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0])
+            {
+                if (board[0, 2] == player)
+                    return +10;
+                else if (board[0, 2] == opponent)
+                    return -10;
+            }
+
+            return 0;
+        }
+
+        // This function returns true if there are moves
+        // remaining on the board. It returns false if
+        // there are no moves left to play.
+        private static Boolean isMovesLeft(string[,] board)
+        {
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    if (board[i, j] == "")
+                        return true;
+            return false;
+        }
+    } 
 }
+
 
