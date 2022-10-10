5bcc5b6e3d887a8d848ba2cf62c461507b94a0c8
Jatin-CI0002 <jatin@caizin.com>
2022-10-10 12:55:11 +0530

    updated file structure

---
+++ TicTacToe.Web/Controllers/GameController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using TicTacToe.Core;
+
+namespace TicTacToe.Web
+{
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+        [Route("api/getindex")]
+        [HttpPost]
+        public IActionResult GetCordinates([FromBody] List<string> characters)
+        {
+            string[,] board = new string[3, 3];
+            int row = 0;
+            int col = 0;
+            foreach(var character in characters)
+            {
+                if (col == 3)
+                {
+                    col = 0;
+                    row++;
+                }
+
+                if (col < 3)
+                    board[row, col++] = character;
+                
+            }
+
+            var cordinates = GameService.findBestMove(board);
+            var data = new
+            {
+                row = cordinates.row,
+                col = cordinates.column
+            };
+            return Ok(data);
+        }
+    }
+}
---
+++ TicTacToe.Web/Startup.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace TicTacToe.Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: MyAllowSpecificOrigins,
+                                  policy =>
+                                  {
+                                      policy.WithOrigins("http://127.0.0.1:5501",
+                                              "http://127.0.0.1:5501").AllowAnyHeader()
+                                                  .AllowAnyMethod();
+                                  });
+            });
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseCors(MyAllowSpecificOrigins);
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
